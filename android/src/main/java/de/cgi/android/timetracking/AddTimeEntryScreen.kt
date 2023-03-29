1383edba1e3fa97712bfd825e477fe6a3dd3bd5e
Robin <robin.holzwarth@stud.h-da.de>
2023-03-29 18:49:55 +0200

    feat: adding time entires via android

--- android/src/main/java/de/cgi/android/timetracking/AddTimeEntryScreen.kt
+++ android/src/main/java/de/cgi/android/timetracking/AddTimeEntryScreen.kt
@@ -6,9 +6,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -16,120 +13,80 @@ import androidx.compose.ui.unit.dp
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import de.cgi.android.destinations.TimeEntryScreenDestination
-import kotlinx.datetime.LocalDate
 import org.koin.androidx.compose.getViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
 fun AddTimeEntryScreen(
-    viewModel: TimeEntryViewModel = getViewModel<TimeEntryViewModel>(),
-    navigator: DestinationsNavigator
+    navigator: DestinationsNavigator,
+    viewModel: AddTimeEntryViewModel = getViewModel<AddTimeEntryViewModel>()
 ) {
-    val scope = rememberCoroutineScope()
-
-    val (day, setDay) = remember { mutableStateOf(LocalDate.) }
-    val (startTime, setStartTime) = remember { mutableStateOf(LocalTime.now()) }
-    val (endTime, setEndTime) = remember { mutableStateOf(LocalTime.now()) }
-    val (project, setProject) = remember { mutableStateOf("") }
-    val (description, setDescription) = remember { mutableStateOf("") }
+    val state = viewModel.state
+    val context = LocalContext.current
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+        modifier = Modifier.padding(16.dp)
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(text = "Day")
-            IconButton(onClick = {
-                scope.launch {
-                    val newDay = showDatePicker(day, scope)
-                    if (newDay != null) {
-                        setDay(newDay)
-                    }
-                }
-            }) {
-                Icon(Icons.Filled.DateRange, contentDescription = "Select Day")
-            }
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(text = "Start Time")
-            IconButton(onClick = {
-                scope.launch {
-                    val newStartTime = showTimePicker(startTime, scope)
-                    if (newStartTime != null) {
-                        setStartTime(newStartTime)
-                    }
-                }
-            }) {
-                Icon(Icons.Filled.Schedule, contentDescription = "Select Start Time")
-            }
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(text = "End Time")
-            IconButton(onClick = {
-                scope.launch {
-                    val newEndTime = showTimePicker(endTime, scope)
-                    if (newEndTime != null) {
-                        setEndTime(newEndTime)
-                    }
-                }
-            }) {
-                Icon(Icons.Filled.Schedule, contentDescription = "Select End Time")
-            }
-        }
-        Spacer(modifier = Modifier.height(16.dp))
+        // Day input field
         OutlinedTextField(
-            value = project,
-            onValueChange = setProject,
+            value = state.day,
+            onValueChange = { viewModel.onEvent(AddTimeEntryUiEvent.TimeEntryDayChanged(it)) },
+            label = { Text(text = "Day") },
+            )
+
+
+        // Start time input field
+        OutlinedTextField(
+            value = state.start,
+            onValueChange = { viewModel.onEvent(AddTimeEntryUiEvent.TimeEntryStartChanged(it)) },
+            label = { Text("Start Time") },
+
+            )
+
+        // End time input field
+        OutlinedTextField(
+            value = state.end,
+            onValueChange = { viewModel.onEvent(AddTimeEntryUiEvent.TimeEntryEndChanged(it)) },
+            label = { Text("End Time") },
+
+            )
+
+        // Project input field
+        OutlinedTextField(
+            value = state.project,
+            onValueChange = { viewModel.onEvent(AddTimeEntryUiEvent.TimeEntryProjectChanged(it)) },
             label = { Text("Project") },
-            singleLine = true
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+
+            )
+
+        // Description input field
         OutlinedTextField(
-            value = description,
-            onValueChange = setDescription,
-            label = { Text("Description") }
-        )
-        Spacer(modifier = Modifier.weight(1f))
+            value = state.description,
+            onValueChange = { viewModel.onEvent(AddTimeEntryUiEvent.TimeEntryDescriptionChanged(it)) },
+            label = { Text("Description") },
+
+            )
+
+        // Submit button
         Button(
             onClick = {
-                viewModel.addTimeEntry(
-                    TimeEntry(
-                        day = day,
-                        startTime = startTime,
-                        endTime = endTime,
-                        project = project,
-                        description = description
-                    )
-                )
-                navigator.navigateUp()
-            },
-            modifier = Modifier.fillMaxWidth(),
-            enabled = project.isNotBlank() && description.isNotBlank()
+                viewModel.onEvent(AddTimeEntryUiEvent.newTimeEntry)
+                navigator.navigate(TimeEntryScreenDestination)},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
         ) {
-            Text(text = "Add")
+            Text("Submit")
+        }
+
+        // Loading indicator
+        if (state.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(24.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
         }
     }
 }
-
-private suspend fun showDatePicker(initialValue: LocalDate, scope: CoroutineScope): LocalDate? {
-    return showDatePickerDialog(
-        initialValue,
-        onDateSelected = { date -> },
-        onCancel = { scope.cancel() }
-    )
-}
-
-private suspend fun showTimePicker(initialValue: LocalTime, scope: CoroutineScope):
